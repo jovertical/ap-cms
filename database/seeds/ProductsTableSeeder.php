6575b7c584d1eb4fad984626c6dc798a7651b67e
Elaine Gumasing <groseelaine@yahoo.com>
2018-05-29 23:58:26 +0800

    description

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,63 +14,63 @@ class ProductsTableSeeder extends Seeder
          \App\Product::create([
             'name' => 'Aura Rich Honey Gold Soap',
             'description' => 'Helps reduce oiliness & delays the appearance of aging.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
 
           \App\Product::create([
             'name' => 'Aura Rich Honey Gold Soap 9',
             'description' => 'Diminishes Wrinkles & Fine lines; Improves skin tone; Minimizes scars, and Moisturizes the skin.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
 
            \App\Product::create([
             'name' => 'Aura Rich Honey Gold Sun Care',
             'description' => 'The Sunscreen with a mixture of collagen is formulated for all skin types. The texture is smooth and light.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
 
             \App\Product::create([
             'name' => 'Aura Rich Aura Honey Gold Cream',
             'description' => 'This facial cream will deeply nourish the skin. Provides protection to harmful pollution. It clears away dark spots.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
 
              \App\Product::create([
             'name' => 'Aura Rich Honey Gold Body Serum',
             'description' => 'The concentrated serum gel with honey & gold extracts regenerate skin. It should be mixed with honey gold lotion or apply directly to skin.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
 
               \App\Product::create([
             'name' => 'Aura Rich Bearberry White Booster Face',
             'description' => 'Nourishes, moisturizes, restores youthfulness of the face to be silky smooth and clear. It removes dark spots and whitens the skin. It does not clog pores. It protect the skin from UVA & UVB rays that causes skin damage.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
 
                \App\Product::create([
             'name' => 'Aura Rich Sunscreen Honey Gold Lotion',
             'description' => 'The sunscreen lotion is concentrated, easily absorbed and non-sticky. It has a mixture of Honey and Gold. The Honey is like a coating film to prevent water loss. The Gold helps prevent aging.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
 
                 \App\Product::create([
             'name' => 'Aura Rich Gold Face Powder SPF 35 PA++',
             'description' => 'Discover the new dimension of face powder with a mixture of Honey, Gold, and Collagen. It helps prevent wrinkles and does not clog pores.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
 
                  \App\Product::create([
             'name' => 'Perfect Face Makeup',
             'description' => 'A full Makeup set with 3 Lipstick Color Shades, 6 Eyeshadow Shades, Blusher, Bronzer, and Highlighter.',
-            'price' => '111',
+            'price' => '0',
             'category_id' => 1,
         ]);
     }
--- resources/views/front/pages/deals.blade.php
+++ resources/views/front/pages/deals.blade.php
@@ -23,20 +23,20 @@
 
 <center>
   <div class="row">
-  <div class="col-sm-4">  <img src="/front/reseller1.jpg"> <br><br><a href="{{ route(user_env().'.getstarted') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><b><i> Reseller Kit # 1 Face Set </b></i><br> 
+  <div class="col-sm-4">  <img src="/front/reseller1.jpg"> <br><br><a href="{{ route(user_env().'.products') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><b><i> Reseller Kit # 1 Face Set </b></i><br> 
   <b><i>"A Complete Whitening Solution for the Face."</b></i><br><br>
   <i>Honey Gold Soap 9,
   Honey Gold Cream,
   Honey Gold Sun Care, <br>
   Bearberry White Booster Face</i><br><br><br><br><br><br>
 </div>
-  <div class="col-sm-4">  <img src="/front/reseller2.jpg"> <br><br><a href="{{ route(user_env().'.getstarted') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><b><i> Reseller Kit # 2 Makeup Set</b></i><br> 
+  <div class="col-sm-4">  <img src="/front/reseller2.jpg"> <br><br><a href="{{ route(user_env().'.products') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><b><i> Reseller Kit # 2 Makeup Set</b></i><br> 
   <i><b>"A Full Makeup Set"</b><br><br>
   Perfect Face Makeup,
   Honey Gold Face Powder</i><br><br><br><br><br><br>
   </div> 
 
-<div class="col-sm-4">  <img src="/front/reseller3.jpg"> <br><br><a href="{{ route(user_env().'.getstarted') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br> <i><b>Reseller Kit # 3 Body Set</i></b><br> 
+<div class="col-sm-4">  <img src="/front/reseller3.jpg"> <br><br><a href="{{ route(user_env().'.products') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br> <i><b>Reseller Kit # 3 Body Set</i></b><br> 
   <i> <b>"A Complete Anti-Aging Solution for the Body."</i> </b><br><br>
   <i>Honey Gold Soap,
   Honey Gold Body Serum,
@@ -46,15 +46,15 @@
 
 <div class="row">
  <center>
- <div class="col-sm-4"> <img src="/front/DistributorSignup.png"> <br><br><a href="{{ route(user_env().'.getstarted') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><b><i> Distributor Signup Pack</b></i> <br> <br>
+ <div class="col-sm-4"> <img src="/front/DistributorSignup.png"> <br><br><a href="{{ route(user_env().'.products') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><b><i> Distributor Signup Pack</b></i> <br> <br>
   <i>Three(3) pieces of each nine(9) AuraRich products.</i> </div>
  </center>
  <center> 
-  <div class="col-sm-4"> <img src="/front/DistributorSample.png"> <br><br><a href="{{ route(user_env().'.getstarted') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><i><b> Distributor Sample Pack</i></b> <br> <br>
+  <div class="col-sm-4"> <img src="/front/DistributorSample.png"> <br><br><a href="{{ route(user_env().'.products') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><i><b> Distributor Sample Pack</i></b> <br> <br>
    <i>Five(5) pieces of each nine(9) AuraRich products. </i></div>
   </center>
   <center>
-  <div class="col-sm-4"> <img src="/front/DistributorStarter.png"> <br><br><a href="{{ route(user_env().'.getstarted') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><i><b> Distributor Starter Pack </i></b><br><br>
+  <div class="col-sm-4"> <img src="/front/DistributorStarter.png"> <br><br><a href="{{ route(user_env().'.products') }}"> <button type="button" class="btn btn-1 select-option"><i class="fa fa-plus"></i>  Reserve Deal</button></a><br><i><b> Distributor Starter Pack </i></b><br><br>
    <i>Ten(10) pieces of each nine(9) AuraRich products.  </i></div>
     </center>
 </div>
