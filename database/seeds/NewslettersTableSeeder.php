917f2de93af17ad1cab5c1690d6a95e3d2f36d8f
Elaine Gumasing <groseelaine@yahoo.com>
2018-05-30 00:12:55 +0800

    description

--- database/seeds/NewslettersTableSeeder.php
+++ database/seeds/NewslettersTableSeeder.php
@@ -12,11 +12,12 @@ class NewslettersTableSeeder extends Seeder
     public function run()
     {
         \App\Newsletter::create([
-            'trigger' => 'subscribed',
+            'trigger' => 'registered',
             'frequency' => null,
             'title' => 'Welcome to '.config('app.name'),
             'body' => 'Aurarich is expanding into the Philippines! Visit our Sell AuraRich page for details on how to register as an Independent Distributor or Reseller. 
-Sign up for our newsletter to stay up to date on launch date events and all other updates to the AuraRich launch.'
+Sign up for our newsletter to stay up to date on launch date events and all other updates to the AuraRich launch.',
+            'file_path' => '/front/newsletter.jpg'
         ]);
     }
 }
